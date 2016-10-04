3552ef83d548ccf77d0c49b2538fab7f84783872
Zonamar <Zonamar@gmail.com>
2016-10-04 12:58:13 -0500

    Starting Point

---
+++ src/caverncrawl/CavernCrawl.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caverncrawl;
+
+/**
+ *
+ * @author zonam_000
+ */
+public class CavernCrawl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+
+        // Init Basic Classes
+        
+        
+        // Open Start Menu
+                // Start Solo Game
+                    // New Character            Character Object Created 
+                    // Load Character           by each of these options
+                // Start Multiplayer Game
+                    // Host
+                        // Add Multiplayer logic
+                    // Join
+                        // Add multiplayer logic
+        
+        
+        // Init Solo Game
+            
+        // Start solo game  (pass Character object)
+        
+        
+        
+        
+    }
+    
+}
